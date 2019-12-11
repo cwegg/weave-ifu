5d97c7b59cb1f53c1e26796d7835d5ec55e17661
lpda <lperalta@ast.cam.ac.uk>
2019-12-11 19:04:45 +0000

    License moved into a comment after the shebang

--- dmurphy/calibstar.py
+++ dmurphy/calibstar.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python3
+
+#
+# Copyright (C) 2018 Cambridge Astronomy Survey Unit
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+
 from astropy.io import fits as pyfits
 import os
 from numpy import unique, arange, random, where
@@ -49,22 +69,6 @@ class CalibStar:
  *  Authors:
  *      David Murphy, Cambridge Astronomical Survey Unit (CASU, IoA)
  *                    dmurphy@ast.cam.ac.uk
- *
- *  Copyright:
- *      Copyright (C) 2018 Cambridge Astronomy Survey Unit.
- *
- *      This program is free software: you can redistribute it and/or modify
- *      it under the terms of the GNU General Public License as published by
- *      the Free Software Foundation, either version 3 of the License, or
- *      (at your option) any later version.
- *
- *      This program is distributed in the hope that it will be useful,
- *      but WITHOUT ANY WARRANTY; without even the implied warranty of
- *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *      GNU General Public License for more details.
- *
- *      You should have received a copy of the GNU General Public License
- *      along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
 +*  
     """
