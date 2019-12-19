658e04097858ad0fcb101804ec62bb17f8b3b8fa
Luis Peralta <lperalta@ast.cam.ac.uk>
2019-12-19 21:58:41 +0000

    Copyright year fixed

--- workflow/stage1/munge.py
+++ workflow/stage1/munge.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# Copyright (C) 2018 Cambridge Astronomical Survey Unit
+# Copyright (C) 2019 Cambridge Astronomical Survey Unit
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
