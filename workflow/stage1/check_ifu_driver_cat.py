785d8138eaf2475d57176ab55e04ec56bb7cfb89
Luis Peralta de Arriba <lperalta@ast.cam.ac.uk>
2020-06-19 17:13:17 +0200

    check_ifu_driver_cat.py fully developed

--- workflow/stage1/check_ifu_driver_cat.py
+++ workflow/stage1/check_ifu_driver_cat.py
@@ -21,7 +21,9 @@
 import argparse
 import logging
 import re
+from collections import OrderedDict
 
+import numpy as np
 from astropy.io import fits
 
 from workflow.utils import check_equal_headers
@@ -39,7 +41,7 @@ def _check_versus_template(cat_filename, template):
 
 def _check_trimester(trimester):
 
-    if re.match('[0-9]{4}[AB][12]', trimester):
+    if re.match('^[0-9]{4}[AB][12]$', trimester):
         result = True
     else:
         result = False
@@ -94,23 +96,37 @@ def _check_ccreport(ccreport):
 
 def _check_targsrvy(array):
 
+    icd_030_targsrvy_set = [
+        'GA-LRDISC', 'GA-LRHIGHLAT', 'GA-HR', 'GA-OC', 'GA-CALIB', 'STEPS',
+        'SCIP-AC', 'SCIP-CYG', 'SCIP-LR', 'WA', 'WC', 'WL-WIDE', 'WL-MID',
+        'WL-DEEP', 'WQ', 'ASTRO-CALIB', 'WD', 'GS', 'ING-SYSCAT'
+    ]
+
     result = True
 
     targsrvy_set = set(array)
 
-    logging.warning(
-        'please, make sure that the following TARGSRV values are allowed in ' +
-        'ICD-030: {}'.format(targsrvy_set)
-    )
+    for value in targsrvy_set:
+        if value not in icd_030_targsrvy_set:
+            logging.error('unexpected TARGSRVY value: {}'.format(value))
+            result = False
 
     if len(targsrvy_set) > 1:
         logging.warning(
             'there is more than one TARGSRV value in the catalogue, ' +
             'are you sure?'
         )
-        
-    if '' in targsrvy_set:
-        logging.error('TARGSRVY value should not be populated by empty strings')
+
+    return result
+
+
+def _check_non_empty_string(array):
+
+    result = True
+
+    set_of_values = set(array)
+
+    if '' in set_of_values:
         result = False
 
     return result
@@ -118,103 +134,521 @@ def _check_targsrvy(array):
 
 def _check_targid(array):
 
-    # Avoid non empty
+    result = _check_non_empty_string(array)
 
-    raise NotImplementedError
+    if result is False:
+        logging.error('TARGID value should not be populated by empty strings')
+
+    return result
 
 
 def _check_targname(array):
 
-    # Avoid non empty
+    result = _check_non_empty_string(array)
 
-    raise NotImplementedError
+    if result is False:
+        logging.error('TARGNAME value should not be populated by empty strings')
+
+    return result
 
 
-def _check_targprio(array):
+def _check_non_null_value(array):
 
-    # Avoid null
-    # Check in range
+    result = True
 
-    raise NotImplementedError
+    for value in array:
+        if np.isnan(value):
+            result = False
+            break
+
+    return result
+
+
+def _get_td(hdu, colname, minmax):
+
+    assert minmax in ['min', 'max']
+
+    if colname in hdu.data.columns.names:
+
+        index = hdu.data.columns.names.index(colname)
+
+        kwd = 'TD{}{}'.format(minmax.upper(), index + 1)
+
+        value = hdu.header[kwd]
+
+    else:
+        value = None
+
+    return value
+
+
+def _check_in_td_range(hdu, colname):
+
+    result = True
+
+    min_value = _get_td(hdu, colname, 'min')
+    max_value = _get_td(hdu, colname, 'max')
+
+    for value in hdu.data[colname]:
+
+        if not np.isnan(value):
+
+            if min_value is not None:
+                if not (value >= min_value):
+                    result = False
+                    break
+
+            if max_value is not None:
+                if not (value <= max_value):
+                    result = False
+                    break
+
+    return result
+
+
+def _check_targprio(hdu):
+
+    result = True
+
+    null_result = _check_non_null_value(hdu.data['TARGPRIO'])
+
+    if null_result is False:
+        logging.error('TARGPRIO values contain NaN')
+        result = False
+
+    range_result = _check_in_td_range(hdu, 'TARGPRIO')
+
+    if range_result is False:
+        logging.error('TARGPRIO values out of range')
+        result = False
+
+    return result
 
 
 def _check_progtemp(array):
 
-    raise NotImplementedError
+    progtemp_regex = '^[4-9][01239][0-9][0-9][12349](\.[0-9]+\+?)?$'
+
+    result = True
+
+    progtemp_set = set(array)
+
+    for value in progtemp_set:
+
+        value_result = True
+
+        if not re.match(progtemp_regex, value):
+            value_result = False
+
+        try:
+            o_char = value[1]
+
+            r_char = value[2]
+            b_char = value[3]
+
+            if o_char == '0':
+                assert r_char not in '12357'
+                assert b_char not in '12357'
+            elif o_char == '1':
+                assert r_char not in '1'
+                assert b_char not in '1'
+            elif o_char == '2':
+                assert r_char not in '15'
+                assert b_char not in '15'
+        except:
+            value_result = False
+
+        if value_result is False:
+            logging.error('unexpected PROGTEMP value: {}'.format(value))
+            result = False
+
+    return result
 
 
 def _check_obstemp(array):
 
-    raise NotImplementedError
+    obstemp_regex = '^[A-X][A-E][A-F][A-E][A-F]$'
+
+    result = True
+
+    obstemp_set = set(array)
+
+    for value in obstemp_set:
+        if not re.match(obstemp_regex, value):
+            logging.error('unexpected OBSTEMP value: {}'.format(value))
+            result = False
+
+    return result
 
 
-def _check_gaia_ra(array):
+def _check_gaia_ra(hdu):
 
-    # Avoid null
-    # Check in range
+    result = True
 
-    raise NotImplementedError
+    null_result = _check_non_null_value(hdu.data['GAIA_RA'])
 
+    if null_result is False:
+        logging.error('GAIA_RA values contain NaN')
+        result = False
 
-def _check_gaia_dec(array):
+    range_result = _check_in_td_range(hdu, 'GAIA_RA')
 
-    # Avoid null
-    # Check in range
+    if range_result is False:
+        logging.error('GAIA_RA values out of range')
+        result = False
 
-    raise NotImplementedError
-
-
-def _check_gaia_epoch(array):
-
-    # Check in range
-
-    raise NotImplementedError
+    return result
 
 
-def _check_gaia_paral(array):
+def _check_gaia_dec(hdu):
 
-    # Check in range
+    result = True
 
-    raise NotImplementedError
+    null_result = _check_non_null_value(hdu.data['GAIA_DEC'])
 
+    if null_result is False:
+        logging.error('GAIA_DEC values contain NaN')
+        result = False
 
-def _check_gaia_ifu_request(array):
+    range_result = _check_in_td_range(hdu, 'GAIA_DEC')
 
-    # Check in range
+    if range_result is False:
+        logging.error('GAIA_DEC values out of range')
+        result = False
 
-    raise NotImplementedError
-
-
-def _check_gaia_ifu_dither(array):
-
-    # Check specific values
-
-    raise NotImplementedError
+    return result
 
 
-def _check_consistency_progtemp_dither(array):
+def _check_gaia_epoch(hdu):
 
-    raise NotImplementedError
+    result = _check_in_td_range(hdu, 'GAIA_EPOCH')
 
+    if result is False:
+        logging.error('GAIA_EPOCH values out of range')
 
-def _check_dither_size(array):
-
-    raise NotImplementedError
-
-
-def _check_custom_dither(array):
-
-    # Check the size depending on the mode
-
-    # Check that some values are the same for different rows
-    # (e.g. ifu_pa_request)
-
-    raise NotImplementedError
+    return result
 
 
-def _check_locations_for_names(array):
+def _check_gaia_pm(hdu):
 
-    raise NotImplementedError
+    result = True
+
+    pmra_array = hdu.data['GAIA_PMRA']
+    pmdec_array = hdu.data['GAIA_PMDEC']
+    epoch_array = hdu.data['GAIA_EPOCH']
+
+    for i, (pmra, pmdec, epoch) in enumerate(
+            zip(pmra_array, pmdec_array, epoch_array)):
+
+        if (not np.isnan(pmra)) or (not np.isnan(pmdec)):
+
+            if np.isnan(pmra) or np.isnan(pmdec) or np.isnan(epoch):
+                logging.error(
+                    'unexpected GAIA_PMRA/GAIA_PMDEC/GAIA_EPOCH values in ' +
+                    'row {}'.format(i + 1))
+                result = False
+    
+    return result
+
+
+def _check_gaia_paral(hdu):
+
+    result = True
+
+    paral_array = hdu.data['GAIA_PARAL']
+    epoch_array = hdu.data['GAIA_EPOCH']
+
+    for i, (paral, epoch) in enumerate(zip(paral_array, epoch_array)):
+
+        if not np.isnan(paral):
+
+            if np.isnan(epoch):
+                logging.error(
+                    'unexpected GAIA_PARAL/GAIA_EPOCH values in row {}'.format(
+                        i + 1))
+                result = False
+    
+    return result
+
+
+def _check_ifu_pa_request(hdu):
+
+    result = True
+
+    range_result = _check_in_td_range(hdu, 'IFU_PA_REQUEST')
+
+    if range_result is False:
+        logging.error('IFU_PA_REQUEST values out of range')
+        result = False
+
+    ifu_pa_request_array = hdu.data['IFU_PA_REQUEST']
+    progtemp_array = hdu.data['PROGTEMP']
+
+    for i, (ifu_pa_request, progtemp) in enumerate(
+            zip(ifu_pa_request_array, progtemp_array)):
+
+        if (not np.isnan(ifu_pa_request)):
+
+            n_char = progtemp[0]
+
+            if n_char not in '456':
+
+                logging.error(
+                    'unexpected non-null IFU_PA_REQUEST for non-LIFU PROGTEMP' +
+                    ' in row {}'.format(i + 1))
+                result = False
+
+    return result
+
+
+def _check_ifu_dither(hdu):
+
+    result = True
+
+    range_result = _check_in_td_range(hdu, 'IFU_DITHER')
+
+    if range_result is False:
+        logging.error('IFU_DITHER values out of range')
+        result = False
+
+    ifu_dither_array = hdu.data['IFU_DITHER']
+    progtemp_array = hdu.data['PROGTEMP']
+
+    for i, (ifu_dither, progtemp) in enumerate(
+            zip(ifu_dither_array, progtemp_array)):
+
+        n_char = progtemp[0]
+
+        if (n_char in '456'):
+            if (ifu_dither not in [-1, 0, 3, 5]):
+                logging.error(
+                    'unexpected IFU_DITHER for LIFU PROGTEMP in row {}'.format(
+                        i + 1))
+                result = False
+        elif (n_char in '789'):
+            if (ifu_dither not in [0, 3, 5]):
+                logging.error(
+                    'unexpected IFU_DITHER for mIFU PROGTEMP in row {}'.format(
+                        i + 1))
+                result = False
+        elif (ifu_dither != 0):
+            logging.error(
+                'unexpected IFU_DITHER and PROGTEMP in row {}'.format(i + 1))
+            result = False
+
+    return result
+
+def _nan_to_none(value):
+
+    if np.isnan(value):
+        result = None
+    else:
+        result = value
+
+    return result
+
+
+def _check_repeated_objects(hdu, offset_tol_arcsec=10.0):
+
+    result = True
+
+    # Create a dictionary with TARGNAME/TARGID as keys and empty lists as values
+
+    targname_array = hdu.data['TARGNAME']
+    targid_array = hdu.data['TARGID']
+
+    object_dict = {(targname, targid): []
+                   for targname, targid in zip(targname_array, targid_array)}
+
+    # Populate the lists with the information of the dithering pattern and
+    # coordinates
+    
+    for i in range(len(hdu.data)):
+
+        targname = hdu.data['TARGNAME'][i]
+        targid = hdu.data['TARGID'][i]
+
+        ifu_dither = hdu.data['IFU_DITHER'][i]
+
+        gaia_ra = hdu.data['GAIA_RA'][i]
+        gaia_dec = hdu.data['GAIA_DEC'][i]
+        gaia_epoch = hdu.data['GAIA_EPOCH'][i]
+        gaia_pmra = hdu.data['GAIA_PMRA'][i]
+        gaia_pmdec = hdu.data['GAIA_PMDEC'][i]
+        gaia_paral = hdu.data['GAIA_PARAL'][i]
+
+        object_dict[(targname, targid)].append(
+            (ifu_dither,
+             (gaia_ra, gaia_dec, _nan_to_none(gaia_epoch),
+              _nan_to_none(gaia_pmra), _nan_to_none(gaia_pmdec),
+              _nan_to_none(gaia_paral)))
+        )
+
+    # Check the uniqueness of the coordinates
+
+    for key in object_dict.keys():
+
+        targname, targid = key
+
+        list_in_value = object_dict[key]
+
+        ifu_dither_list = [elem[0] for elem in list_in_value]
+        coord_list = [elem[1] for elem in list_in_value]
+
+        # Case 1: We do not have custom dithers
+        if (-1 not in ifu_dither_list):
+            if len(set(coord_list)) != 1:
+                logging.error(
+                    'unexpected change of coordinates in non-custom dither {}:{}'.format(
+                        targname, targid))
+                import ipdb
+                ipdb.set_trace()
+                result = False
+        else:
+
+            # If we have a mix of non-custom and custom dithers, take the non-
+            # custom as reference. If we have only custom dithers, take the
+            # first position as reference
+            
+            non_custom_coord_list = [elem[1] for elem in list_in_value
+                                     if elem[0] != -1]
+
+            non_custom_coord_set = set(non_custom_coord_list)
+
+            # All the non-custom dithers should have the same coordinates
+            
+            if len(non_custom_coord_set) > 1:
+                logging.error(
+                    'unexpected change of coordinates for {}:{}'.format(
+                        targname, targid))
+                result = False
+                continue
+            elif len(non_custom_coord_set) == 1:
+                ref_coord = non_custom_coord_list[0]
+            else:
+                ref_coord = coord_list[0]
+
+            # Check that the offsets are not far from the reference
+            
+            for ifu_dither, coord in list_in_value:
+                
+                if ifu_dither == -1:
+
+                    ra_offset = np.abs((coord[0] - ref_coord[0]) *
+                                       np.cos(np.deg2rad(ref_coord[1]))) * 3600
+                    dec_offset = np.abs(coord[1] - ref_coord[1]) * 3600
+
+                    if ((ra_offset > offset_tol_arcsec) or
+                        (dec_offset > offset_tol_arcsec)):
+                        logging.error(
+                            'too big offset for custom dither in {}:{}'.format(
+                                targname, targid))
+                        result = False
+                        continue
+
+                    for i in range(2, 6):
+                        if ((coord[i] != ref_coord[i]) and not
+                            (np.isnan(coord[i]) and np.isnan(ref_coord[i]))):
+                            logging.error(
+                                'unexpected change of GAIA_EPOCH/GAIA_PMRA/'
+                                'GAIA_PMDEC/GAIA_PARAL for {}:{}'.format(
+                                    targname, targid))
+                            result = False
+                            continue
+
+    return result
+
+
+def _progtemp_to_num_exp(progtemp):
+
+    o_char = progtemp[1]
+    r_char = progtemp[2]
+    b_char = progtemp[3]
+
+    convert_dict = {
+        '0': {'0': 1, '1': None, '2': None, '3': None, '4': 2,
+              '5': None, '6': 3, '7': None, '8': 4, '9': 5},
+        '1': {'0': 1, '1': None, '2': 2, '3': 3, '4': 4,
+              '5': 5, '6': 6, '7': 7, '8': 8, '9': 9},
+        '2': {'0': 1, '1': None, '2': 3, '3': 5, '4': 6,
+              '5': None, '6': 9, '7': 10, '8': 12, '9': 15},
+        '3': {'0': 1, '1': 2, '2': 4, '3': 6, '4': 8,
+              '5': 10, '6': 12, '7': 14, '8': 16, '9': 20}
+    }
+
+    if o_char in convert_dict.keys():
+        num_exp_r = convert_dict[o_char][r_char]
+        num_exp_b = convert_dict[o_char][b_char]
+    else:
+        num_exp_r = None
+        num_exp_b = None
+
+    result = (num_exp_r, num_exp_b)
+
+    return result
+
+
+def _check_progtemp_ifu_dither_compatibility(hdu):
+
+    result = True
+
+    progtemp_array= hdu.data['PROGTEMP']
+    ifu_dither_array = hdu.data['IFU_DITHER']
+
+    # Check the non-custom dithers
+
+    for i, (progtemp, ifu_dither) in enumerate(
+            zip(progtemp_array, ifu_dither_array)):
+
+        if ifu_dither > 1:
+
+            num_dither = ifu_dither
+            num_exp_r, num_exp_b = _progtemp_to_num_exp(progtemp)
+ 
+            if ((num_exp_r % num_dither != 0) or 
+                (num_exp_b % num_dither != 0)):
+                logging.error(
+                    'PROGTEMP and IFU_DITHER are not compatible in row {}'.format(
+                        i + 1))
+                result = False
+
+    # Chech the custom dithers
+
+    targname_array = hdu.data['TARGNAME']
+    targid_array = hdu.data['TARGID']
+    obstemp_array = hdu.data['OBSTEMP']
+
+    custom_dict = OrderedDict()
+
+    for i, (ifu_dither, targname, targid, progtemp, obstemp) in enumerate(
+            zip(ifu_dither_array,
+                targname_array, targid_array, progtemp_array, obstemp_array)):
+
+        if ifu_dither == -1:
+
+            key = (targname, targid, progtemp, obstemp)
+
+            if key in custom_dict:
+                custom_dict[key] += 1
+            else:
+                custom_dict[key] = 1
+
+    for key in custom_dict.keys():
+
+        targname, targid, progtemp, obstemp = key
+
+        num_dither = custom_dict[key]
+        num_exp_r, num_exp_b = _progtemp_to_num_exp(progtemp)
+
+        if ((num_exp_r % num_dither != 0) or 
+            (num_exp_b % num_dither != 0)):
+            logging.error(
+                'PROGTEMP and custom dither {}:{}:{}:{} are not compatible'.format(
+                    targname, targid, progtemp, obstemp)
+            )
+            result = False
+
+    return result
 
 
 def check_ifu_driver_cat(cat_filename, template=None, check_vs_template=True):
@@ -290,9 +724,54 @@ def check_ifu_driver_cat(cat_filename, template=None, check_vs_template=True):
 
         # Check the data in the table of its first extension
 
+        hdu1 = hdu_list[1]
         data = hdu_list[1].data
 
         if not _check_targsrvy(data['TARGSRVY']):
+            result = False
+
+        # Nothing to check in TARGPROG
+
+        if not _check_targid(data['TARGID']):
+            result = False
+
+        if not _check_targname(data['TARGNAME']):
+            result = False
+
+        if not _check_targprio(hdu1):
+            result = False
+
+        if not _check_progtemp(data['PROGTEMP']):
+            result = False
+
+        if not _check_obstemp(data['OBSTEMP']):
+            result = False
+
+        if not _check_gaia_ra(hdu1):
+            result = False
+
+        if not _check_gaia_dec(hdu1):
+            result = False
+
+        if not _check_gaia_epoch(hdu1):
+            result = False
+
+        if not _check_gaia_pm(hdu1):
+            result = False
+
+        if not _check_gaia_paral(hdu1):
+            result = False
+
+        if not _check_ifu_pa_request(hdu1):
+            result = False
+
+        if not _check_ifu_dither(hdu1):
+            result = False
+
+        if not _check_repeated_objects(hdu1):
+            result = False
+
+        if not _check_progtemp_ifu_dither_compatibility(hdu1):
             result = False
 
     return result
