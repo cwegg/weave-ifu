ad3126ad0b5715fe62b0dae3b2c3c59d09f8c81a
Luis Peralta de Arriba <lperalta@ast.cam.ac.uk>
2020-07-02 17:50:21 +0200

    init files updated for the first 3 stages

--- workflow/__init__.py
+++ workflow/__init__.py
@@ -1,3 +1,10 @@
-from . import stage1
 from . import utils
+
+from . import stage1
+from . import stage2
+from . import stage3
+
+from .stage1 import create_ifu_driver_cat as stage1_main
+from .stage2 import create_xml_files as stage2_main
+from .stage3 import add_guide_and_calib_stars as stage3_main
 
