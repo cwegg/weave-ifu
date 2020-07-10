f9fc43d2b0703d5a76cbc038716dc18dd6ac9abc
Luis Peralta de Arriba <lperalta@ast.cam.ac.uk>
2020-07-10 15:40:42 +0200

    init files improved

--- workflow/__init__.py
+++ workflow/__init__.py
@@ -4,7 +4,18 @@ from . import stage1
 from . import stage2
 from . import stage3
 
+from . import stage5
+
+from . import stage7
+
+
 from .stage1 import create_ifu_driver_cat as stage1_main
+from .stage1 import check_ifu_driver_cat as stage1_check
+from .stage1 import plot_ifu_driver_cat as stage1_plot
 from .stage2 import create_xml_files as stage2_main
 from .stage3 import add_guide_and_calib_stars as stage3_main
+
+from .stage5 import create_ifu_fits_cat as stage5_main
+
+from .stage7 import create_combo_fits_cat as stage7_main
 
--- workflow/stage1/__init__.py
+++ workflow/stage1/__init__.py
@@ -1,5 +1,7 @@
 from .create_ifu_driver_template import create_ifu_driver_template
 from .create_ifu_driver_cat import create_ifu_driver_cat
+from .check_ifu_driver_cat import check_ifu_driver_cat
+from .plot_ifu_driver_cat import plot_ifu_driver_cat
 from ._create_ifu_driver_cat_example import set_keywords_info \
     as _set_keywords_info_for_example
 from ._create_ifu_driver_cat_example import get_data_dict \
@@ -8,6 +10,4 @@ from ._create_ifu_driver_cat_cheating import get_keywords_info \
     as _get_keywords_info_for_cheating
 from ._create_ifu_driver_cat_cheating import get_data_dict \
     as _get_data_dict_for_cheating
-from .check_ifu_driver_cat import check_ifu_driver_cat
-from .plot_ifu_driver_cat import plot_ifu_driver_cat
 
